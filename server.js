9845b26bc68573ba08422fc27a0db6448519bc1e
Austin F <austindanielfrench@gmail.com>
2017-12-28 21:23:36 -0800

    created a simple handlebars template -- working!

---
+++ server.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var server = express();
+var exphbs = require('express-handlebars');
+var request = require('request');
+var async = require('async');
+
+server.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+server.set('view engine', 'handlebars');
+
+
+server.get('/', (req, res) => {
+  const data = {};
+  const api_key = 'RGAPI-e504135e-08e0-4a0d-b3e6-964615874018';
+  const summonerInput = 'im frog';
+  const URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summonerInput + '?api_key=' + api_key;
+
+
+  async.waterfall([
+    function(callback) {
+      request(URL, function(err, response, body) {
+        if(!err && response.statusCode == 200) {
+          var json = JSON.parse(body);
+          console.log(json)
+          console.log(summonerInput);
+          data.id = json.id;
+          data.name = json.name;
+          callback(null, data);
+        } else {
+          console.log(err);
+        }
+      });
+    }
+  ],
+  function(err, data) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    res.render('index', {
+      info: data
+    });
+  });
+});
+
+var port = Number(process.env.PORT || 3000);
+server.listen(port, () => {
+  console.log('server up');
+});
