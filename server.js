17716ad543ebbb503c85e14efa23846ffa06e465
Austin F <austindanielfrench@gmail.com>
2017-12-28 21:34:46 -0800

    went from hardcoded user to user input being possible

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ server.engine('handlebars', exphbs({defaultLayout: 'main'}));
 server.set('view engine', 'handlebars');
 
 
-server.get('/', (req, res) => {
+server.get('/:summonerId', (req, res) => {
+  const { summonerId } = req.params;
   const data = {};
   const api_key = 'RGAPI-e504135e-08e0-4a0d-b3e6-964615874018';
-  const summonerInput = 'im frog';
-  const URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summonerInput + '?api_key=' + api_key;
+  const URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summonerId + '?api_key=' + api_key;
 
 
   async.waterfall([
@@ -22,9 +22,12 @@ server.get('/', (req, res) => {
         if(!err && response.statusCode == 200) {
           var json = JSON.parse(body);
           console.log(json)
-          console.log(summonerInput);
+          console.log(summonerId);
           data.id = json.id;
           data.name = json.name;
+          data.accountId = json.accountId;
+          data.profileIconId = json.profileIconId;
+          data.summonerLevel = json.summonerLevel;
           callback(null, data);
         } else {
           console.log(err);
