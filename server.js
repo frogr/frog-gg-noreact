4ce8955ef44f6ae03d885ad047fcd0b8867745a5
Austin F <austindanielfrench@gmail.com>
2018-01-01 21:42:34 -0800

    WORKING FOR RANKED INFO!

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const async = require('async');
 const LeagueJs = require('leaguejs');
 const fetch = require('node-fetch');
 
-const API_KEY = 'RGAPI-e504135e-08e0-4a0d-b3e6-964615874018'
+const API_KEY = 'RGAPI-dbacd99f-6cce-44e3-887f-e2a674cc35ce';
 
 const api = new LeagueJs(API_KEY);
 
@@ -18,16 +18,14 @@ server.set('view engine', 'handlebars');
 server.get('/:summonerId', (req, res) => {
   const { summonerId } = req.params;
   const data = {};
-  const ACC_DATA_ACC_DATA_URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summonerId + '?api_key=' + API_KEY;
-  const RANKS_URL = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/' + accountId + '?api_key=' + API_KEY;
+  const ACC_DATA_URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + summonerId + '?api_key=' + API_KEY;
 
   async.waterfall([
     function(callback) {
       request(ACC_DATA_URL, function(err, response, body) {
         if(!err && response.statusCode == 200) {
-          var json = JSON.parse(body);
-          console.log(json)
-          console.log(summonerId);
+          let json = JSON.parse(body);
+          console.log("FIRST JSON", json)
           data.id = json.id;
           data.name = json.name;
           data.accountId = json.accountId;
@@ -39,8 +37,30 @@ server.get('/:summonerId', (req, res) => {
         }
       });
     },
-    function (callback) {
-      request()
+    function (data, callback) {
+      const RANKS_URL = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/' + data.id + '?api_key=' + API_KEY;
+      request(RANKS_URL, (err, res, body) => {
+        if (!err && res.statusCode === 200) {
+          let json = JSON.parse(body);
+          console.log("SECOND JSON", json);
+          let i = 0
+          if (json[i].queueType !== 'RANKED_SOLO_5x5') {
+            i++
+          }
+          data.leagueName = json[i].leagueName;
+          data.tier = json[i].tier;
+          data.rank = json[i].rank;
+          data.wins = json[i].wins;
+          data.losses = json[i].losses;
+          data.veteran = json[i].veteran;
+          data.inactive = json[i].inactive;
+          data.freshBlood = json[i].freshBlood;
+          data.hotStreak = json[i].hotStreak;
+          callback(null, data);
+        } else {
+          console.log(err);
+        }
+      });
     }
   ],
   function(err, data) {
@@ -55,7 +75,7 @@ server.get('/:summonerId', (req, res) => {
   });
 });
 
-var port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT || 3000);
 server.listen(port, () => {
   console.log('server up');
 });
